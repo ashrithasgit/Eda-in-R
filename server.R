0a5383bdef8b215d9e53c738120e3c7f9e47d757
ashrithasgit <159109155+ashrithasgit@users.noreply.github.com>
2024-12-18 09:06:53 +0530

    Update server.R

--- server.R
+++ server.R
@@ -29,7 +29,7 @@ server <- function(input, output, session) {
   
   output$table <- renderDT({
     req(data())
-    datatable(data(), options = list(pageLength = 5))
+    datatable(data(), options = list(pageLength = 10))
   })
   
   
